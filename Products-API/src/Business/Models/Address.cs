5e36e462d395a9182cc9eed93ca47e82c059a9b1
Bruno Knischewski <knischewski@gmail.com>
2022-06-30 16:33:00 -0300

    feat: created models

---
+++ Products-API/src/Business/Models/Address.cs
@@ -0,0 +1,16 @@
+﻿namespace Business.Models
+{
+    public class Address : Entity
+    {
+        public Guid SupplierId { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string Complement { get; set; }
+        public string ZipCode { get; set; }
+        public string Distric { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        public Supplier Supplier { get; set; }
+    }
+}
---
+++ Products-API/src/Business/Models/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace Business.Models
+{
+    public class Entity
+    {
+        public Guid Id { get; set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
