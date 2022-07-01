40aec9d86844be736e9f00e95d9f83bf22a4a2e3
Bruno Knischewski <knischewski@gmail.com>
2022-07-01 18:24:52 -0300

    feat: automapper

--- Products-API/src/API/Program.cs
+++ Products-API/src/API/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<ProductDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddAutoMapper(typeof(Program));
+
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
